b1985c7fc34fd3ce58c0e65e45ac0dc48718b759
aldochaman <76443326+aldochaman@users.noreply.github.com>
2021-07-01 14:52:32 -0500

    Add files via upload

---
+++ Framework.CrystalReports/Columns.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.CrystalReports
+{
+     /// <summary>
+     /// Columns
+     /// </summary>
+     public class Columns
+     {
+          /// <summary>
+          /// Gets or sets a value indicating whether this instance is numeric.
+          /// </summary>
+          /// <value>
+          ///   <c>true</c> if this instance is numeric; otherwise, <c>false</c>.
+          /// </value>
+          public bool IsNumeric { get; set; }
+          /// <summary>
+          /// Gets or sets the length.
+          /// </summary>
+          /// <value>
+          /// The length.
+          /// </value>
+          public int Length { get; set; }
+     }
+}
---
+++ Framework.CrystalReports/FormatType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.CrystalReports
+{
+     /// <summary>
+     /// Supported export formats for Crystal reports.
+     /// </summary>
+     public enum FormatType
+     {
+          PDF = 0,
+          XLSX = 1,
+          DOCX = 2,
+          DOC = 3,
+          XML = 4,
+          CSV = 5,
+          HTML = 6
+     }
+}
